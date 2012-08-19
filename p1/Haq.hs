87716786c946a98bea0fccc05304b45e3cf361ce
Aaron Peschel <aaron.peschel@gmail.com>
2012-08-19 15:51:49 -0700

    Remove accidentally copied license information.

--- p1/Haq.hs
+++ p1/Haq.hs
@@ -1,10 +1,5 @@
---
--- Copyright (c) 2006 Don Stewart - http://www.cse.unsw.edu.au/~dons/
--- GPL version 2 or later (see http://www.gnu.org/copyleft/gpl.html)
---
 import System.Environment
 
--- | 'main' runs the main program
 main :: IO ()
 main = print . haqify $ [1..999]
 
